89075c13e0ea58206d090cd4720ff7ca90cc8ad7
Filip Vrubel <114007566+FilipVrubel@users.noreply.github.com>
2026-03-14 16:02:48 +0100

    fix: update stardist.groovy to match api

--- scripts/stardist.groovy
+++ scripts/stardist.groovy
@@ -1,18 +1,24 @@
 import qupath.ext.stardist.StarDist2D
+import java.nio.file.Files
+import java.nio.file.Paths
 
-// 'api', 'imageData', 'hierarchy' are injected by the Java script runner
+// Injected by the Java script runner: api, imageData, hierarchy, server, selectionModel, project, args
 
+api.reportProgress(0.0)
 def inputRoi = api.getInputRoi()
-if (inputRoi == null) {
-    api.fail("No input_roi provided by EMPAIA")
+if (inputRoi == null)
+    println "No input_roi provided, running StarDist on full image"
+else
+    println "Using input_roi: ${inputRoi.getROI()}"
+
+def modelPath = "./stardist_models/dsb2018_heavy_augemnt"
+
+if (!Files.exists(Paths.get(modelPath))) {
+    println "Model path does not exist: ${modelPath}"
     return
 }
 
-def modelPath = System.getenv('STARDIST_MODEL_PATH')
-if (!modelPath) {
-    api.fail("STARDIST_MODEL_PATH environment variable not set")
-    return
-}
+api.reportProgress(0.1)
 
 def stardist = StarDist2D.builder(modelPath)
     .normalizePercentiles(1, 99)
@@ -22,10 +28,17 @@ def stardist = StarDist2D.builder(modelPath)
     .build()
 
 println "Running StarDist detection..."
-stardist.detectObjects(imageData, [inputRoi])
+if (inputRoi == null)
+    stardist.detectObjects(imageData)
+else
+    stardist.detectObjects(imageData, [inputRoi])
+
+api.reportProgress(0.8)
 
 def detections = hierarchy.getDetectionObjects()
 println "Detected ${detections.size()} objects"
 
-api.postAnnotations("cell_detections", detections)
-api.postValues("cell_count", [detections.size()])
+api.postAnnotations("output_annotations", detections)
+api.reportProgress(0.9)
+api.postValues("output_values", [detections.size()])
+api.reportProgress(1.0)
